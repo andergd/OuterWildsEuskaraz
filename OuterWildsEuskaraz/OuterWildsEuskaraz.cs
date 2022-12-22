c188de3e328c312ae6b5a2fe2b7233a374383191
andergd <andergd@bizkaia.eu>
2022-12-22 14:38:03 +0100

    Agregar archivos de proyecto.

---
+++ OuterWildsEuskaraz/OuterWildsEuskaraz.cs
@@ -0,0 +1,30 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+
+namespace OuterWildsEuskaraz
+{
+    public class OuterWildsEuskaraz : ModBehaviour
+    {
+        private void Awake()
+        {
+            // You won't be able to access OWML's mod helper in Awake.
+            // So you probably don't want to do anything here.
+            // Use Start() instead.
+        }
+
+        private void Start()
+        {
+            // Starting here, you'll have access to OWML's mod helper.
+            ModHelper.Console.WriteLine($"My mod {nameof(OuterWildsEuskaraz)} is loaded!", MessageType.Success);
+
+
+            // Example of accessing game code.
+            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+            {
+                if (loadScene != OWScene.SolarSystem) return;
+                ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+            };
+        }
+    }
+
+}
